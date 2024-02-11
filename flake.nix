7d165a29b6a15dbe4b305fdb41cba3e9d5ca10a6
Elijah M. Immer <me@eimmer.me>
2024-02-10 23:30:13 -0800

    remove nixos module, it will work better just to make a service yourself.

--- flake.nix
+++ flake.nix
@@ -21,96 +21,47 @@
       # no mac, wayland isn't on mac (as far as I know...)
       # also, bsd users can fix this themselves. There are too many options...
     ];
-  in
-    (flake-utils.lib.eachSystem supportedSystems (system: let
-      pkgs = (import nixpkgs) {
-        inherit system;
-      };
-
-      naersk' = pkgs.callPackage naersk {};
-
-      buildInputs = with pkgs; [
-        makeBinaryWrapper
-        pkg-config
-        glib
-        gtk4
-        gtk4-layer-shell
-      ];
-    in {
-      packages.default = naersk'.buildPackage {
-        inherit buildInputs;
-        src = ./.;
-        meta = with pkgs.lib; {
-          description = "A GTK4 status bar. in rust.";
-          homepage = "https://github.com/elijahimmer/bar-rs";
-          license = licenses.mit;
-          mainProgram = "bar-rs";
-        };
-        
-        postInstall = ''
-          wrapProgram $out/bin/bar-rs \
-            --prefix PATH : ${nixpkgs.lib.makeBinPath [ nixpkgs.legacyPackages.${system}.pw-volume ]}
-        '';
-      };
-
-      devShells.default = pkgs.mkShell {
-        buildInputs =
-          buildInputs
-          ++ (with pkgs; [
-            cargo
-            rustc
-            clippy
-          ]);
-      };
-
-      formatter = pkgs.alejandra;
-    }))
-    // {
-      nixosModules = rec {
-        bar-rs = default;
-        default = {
-          config,
-          lib,
-          pkgs,
-          system,
-          ...
-        }: let
-          cfg = config.services.bar-rs;
-        in
-          with lib; {
-            options = {
-              services.bar-rs = {
-                enable = mkOption {
-                  type = types.bool;
-                  default = false;
-                  example = true;
-                  description = mdDoc ''
-                    Enables bar-rs to run when your window manager starts
-                  '';
-                };
-                targets = mkOption {
-                  type = types.listOf types.str;
-                  default = ["graphical-session.target"];
-                  example = ["hyprland-session.target"];
-                };
-              };
-            };
-
-            config = mkIf cfg.enable {
-              systemd = {
-                user = {
-                  services.bar-rs = {
-                    wantedBy = cfg.targets;
-                    script = getExe self.packages.${system}.default;
-                    reloadIfChanged = true;
-                    environment = {
-                      pw-volume = pkgs.pw-volume;
-                    };
-                  };
-                };
-              };
-            };
-          };
-      };
+  in (flake-utils.lib.eachSystem supportedSystems (system: let
+    pkgs = (import nixpkgs) {
+      inherit system;
     };
+
+    naersk' = pkgs.callPackage naersk {};
+
+    buildInputs = with pkgs; [
+      makeBinaryWrapper
+      pkg-config
+      glib
+      gtk4
+      gtk4-layer-shell
+    ];
+  in {
+    packages.default = naersk'.buildPackage {
+      inherit buildInputs;
+      src = ./.;
+      meta = with pkgs.lib; {
+        description = "A GTK4 status bar. in rust.";
+        homepage = "https://github.com/elijahimmer/bar-rs";
+        license = licenses.mit;
+        mainProgram = "bar-rs";
+      };
+
+      postInstall = ''
+        wrapProgram $out/bin/bar-rs \
+          --prefix PATH : ${nixpkgs.lib.makeBinPath [nixpkgs.legacyPackages.${system}.pw-volume]}
+      '';
+    };
+
+    devShells.default = pkgs.mkShell {
+      buildInputs =
+        buildInputs
+        ++ (with pkgs; [
+          cargo
+          rustc
+          clippy
+        ]);
+    };
+
+    formatter = pkgs.alejandra;
+  }));
 }
